64af4e994038a736bc2f018eccbc9f7bfcb16863
Alam-Monir <monirrza0641@gmail.com>
2025-03-04 15:11:41 +0530

    minor fixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Chongkhlenggraphics</title>
+    <title>ChongkhlengGraphics</title>
     <style>
         .test {
             display: flex;
