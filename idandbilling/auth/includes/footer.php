d44ab03f89f31746645cbf999d3a613bd669b8c0
landdebbarma <landdebbarma5@gmail.com>
2025-03-04 22:38:34 +0530

    fix

--- idandbilling/auth/includes/footer.php
+++ idandbilling/auth/includes/footer.php
@@ -1,5 +1,5 @@
-<script src="/idandbilling/assets/js/bootstrap.bundle.min.js"></script>
-<script src="/idandbilling/assets/js/jquery-3.7.1.min.js"></script>
+<script src="/chongkhlenggraphics/assets/js/bootstrap.bundle.min.js"></script>
+<script src="/chongkhlenggraphics/assets/js/jquery-3.7.1.min.js"></script>
 
 </body>
 
--- idandbilling/auth/includes/header.php
+++ idandbilling/auth/includes/header.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Login Or Signup</title>
-  <link href="/idandbilling/assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="/idandbilling/assets/css/bootstrap-icons.min.css" rel="stylesheet" />
+  <link href="/chongkhlenggraphics/assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="/chongkhlenggraphics/assets/css/bootstrap-icons.min.css" rel="stylesheet" />
 </head>
 
 <body>
